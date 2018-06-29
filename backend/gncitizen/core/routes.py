3fd2f7b27f239cad865272db61c9cc830beabdcc
lpofredc <frederic.cloitre@lpo.fr>
2018-06-30 01:06:21 +0200

    User authentification with jwt

---
+++ backend/gncitizen/core/routes.py
@@ -0,0 +1,6 @@
+from server import app, db
+from flask import jsonify
+
+@app.route('/')
+def index():
+     return jsonify({'message': 'Hello, World!'})
--- backend/server.py
+++ backend/server.py
@@ -1,9 +1,43 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
 
-@app.route("/")
-def home():
-    """
-    Adresse d'accueuil
-    """
-    return "GeoNature-citizen Accueuil"
+app = Flask(__name__)
+api = Api(app)
+
+
+# Configuration de la bdd
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'maclesecrete'
+
+db = SQLAlchemy(app)
+
+# JWTManager
+app.config['JWT_SECRET_KEY'] = 'jwt-secret-string'
+app.config['JWT_BLACKLIST_ENABLED'] = True
+app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
+
+jwt = JWTManager(app)
+
+from gncitizen.core import routes, models, resources
+
+
+@jwt.token_in_blacklist_loader
+def check_if_token_in_blacklist(decrypted_token):
+    jti = decrypted_token['jti']
+    return models.RevokedTokenModel.is_jti_blacklisted(jti)
+
+api.add_resource(resources.UserRegistration, '/registration')
+api.add_resource(resources.UserLogin, '/login')
+api.add_resource(resources.UserLogoutAccess, '/logout/access')
+api.add_resource(resources.UserLogoutRefresh, '/logout/refresh')
+api.add_resource(resources.TokenRefresh, '/token/refresh')
+api.add_resource(resources.AllUsers, '/users')
+api.add_resource(resources.SecretResource, '/secret')
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
