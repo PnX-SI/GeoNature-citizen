f2d4c2f332b810c3d51a6c71f90a192a8b834995
fred.lpo07 <frederic.cloitre@lpo.fr>
2018-07-18 18:48:14 +0200

    update models

--- backend/gncitizen/auth/models.py
+++ backend/gncitizen/auth/models.py
@@ -27,9 +27,13 @@ class UserModel(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    surname = db.Column(db.String(100), unique=True, nullable=False)
     username = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
-    email = db.Column(db.String(150), nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    phone = db.Column(db.String(15))
+    organism = db.Column(db.String(100))
     admin = db.Column(db.Boolean, default=False)
 
     def save_to_db(self):
@@ -55,16 +59,17 @@ class UserModel(db.Model):
                 'username': x.username,
                 'password': x.password,
                 'email': x.email,
+                'phone': x.phone,
                 'admin': x.admin
             }
 
-        return {'users': list(map(lambda x: to_json(x), UserModel.query.all()))}
+            return {'users': list(map(lambda x: to_json(x), UserModel.query.all()))}
 
-    @classmethod
-    def delete_all(cls):
-        try:
-            num_rows_deleted = db.session.query(cls).delete()
-            db.session.commit()
-            return {'message': '{} row(s) deleted'.format(num_rows_deleted)}
-        except:
-            return {'message': 'Something went wrong'}
+        @classmethod
+        def delete_all(cls):
+            try:
+                num_rows_deleted = db.session.query(cls).delete()
+                db.session.commit()
+                return {'message': '{} row(s) deleted'.format(num_rows_deleted)}
+            except:
+                return {'message': 'Something went wrong'}
--- backend/gncitizen/sights/models.py
+++ backend/gncitizen/sights/models.py
@@ -4,13 +4,15 @@
 from datetime import datetime
 
 from server import db
+from geoalchemy2 import Geometry
 
 
 class SpecieModel(db.Model):
     """Table des Espèce"""
     __tablename__ = 'species'
-    id = db.Column(db.Integer, primary_key=True)
-    cd_ref = db.Column(db.Integer)
+    __table_args__ = {'schema': 'gn_citizen'}
+    id_specie = db.Column(db.Integer, primary_key=True)
+    cd_nom = db.Column(db.Integer)
     common_name = db.Column(db.String(150))
     sci_name = db.Column(db.String(150))
 
@@ -18,12 +20,20 @@ class SpecieModel(db.Model):
 class SightModel(db.Model):
     """Table des observations"""
     __tablename__ = 'sights'
-    id = db.Column(db.Integer, primary_key=True)
-    cd_ref = db.Column(db.Integer, db.ForeignKey('species.cd_ref'))
+    __table_args__ = {'schema': 'gn_citizen'}
+    id_sight = db.Column(db.Integer, primary_key=True)
+    uuid_sinp = db.Column(db.String(200), nullable=False)
+    cd_nom = db.Column(db.Integer, db.ForeignKey('species.cd_nom'))
     specie = db.relationship(
         'SpecieModel',
         backref=db.backref('specie', lazy='dynamic'),
     )
-    # dateobs = db.Column(db.DATETIME, nullable=False)
+    date = db.Column(db.DATE, nullable=False)
+    id_role = db.Column(db.Integer)
+    obs_txt = db.Column(db.String(150))
+    email = db.Column(db.String(150))
+    phone = db.Column(db.String(150))
     count = db.Column(db.Integer)
+    comment = db.Column(db.String(300))
+    geom = db.Column(Geometry('POINT', 4326))
     posted_at = db.Column(db.DATETIME, nullable=False, default=datetime.utcnow)
