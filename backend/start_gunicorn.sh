6caea4bedc2037b4860d4ea88a77411f4ca789d0
lpofredc <frederic.cloitre@lpo.fr>
2018-09-02 01:21:34 +0200

    add start_gunicorn.sh

---
+++ backend/start_gunicorn.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FLASKDIR=$(readlink -e "${0%/*}")
+APP_DIR="$(dirname "$FLASKDIR")"
+venv_dir="venv"
+
+echo "Starting $app_name"
+echo "$FLASKDIR"
+echo $APP_DIR
+
+# activate the virtualenv
+source $FLASKDIR/$venv_dir/bin/activate
+
+cd $FLASKDIR
+
+# Start your gunicorn
+exec mkdir -p $APP_DIR/var/log/
+exec gunicorn  wsgi:app --error-log $APP_DIR/var/log/gn_errors.log --pid="geonature-citizen.pid" -b "0.0.0.0:5101"  -n "geonature-citizen"
