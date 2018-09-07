cb4dfe3b244c1ad01b0e0c0bf6f64e021408f671
lpofredc <frederic.cloitre@lpo.fr>
2018-09-07 21:32:25 +0200

    update start_gunicorn, now check if log_dir exists. if not, the script create it

--- backend/start_gunicorn.sh
+++ backend/start_gunicorn.sh
@@ -5,14 +5,23 @@ APP_DIR="$(dirname "$FLASKDIR")"
 venv_dir="venv"
 
 echo "Starting $app_name"
-echo "$FLASKDIR"
-echo $APP_DIR
+echo "FLASKDIR: $FLASKDIR"
+echo "APP_DIR: $APP_DIR"
 
 # activate the virtualenv
 source $FLASKDIR/$venv_dir/bin/activate
+echo "VENV: $FLASKDIR/$venv_dir/bin/activate"
 
 cd $FLASKDIR
 
 # Start your gunicorn
-exec mkdir -p $APP_DIR/var/log/
-exec gunicorn --error-log $APP_DIR/var/log/gn_errors.log --pid="geonature-citizen.pid" -b "0.0.0.0:5001"  -n "geonature-citizen" wsgi:app
+LOG_DIR="$APP_DIR/var/log"
+echo "LOAG_DIR: $LOG_DIR"
+if [[ ! -e $LOG_DIR ]]; then
+    mkdir -p $LOG_DIR
+elif [[ ! -d $LOG_DIR ]]; then
+    echo "LOG_DIR already exists but is not a directory" 1>&2
+fi
+
+echo "Starting gunicorn"
+exec gunicorn --error-log $APP_DIR/var/log/gn_errors.log --pid="geonature-citizen.pid" -b 0.0.0.0:5001  -n "geonature-citizen" wsgi:app
