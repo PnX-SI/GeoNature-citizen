f0ab363996d029cbc19d3d45a87913ee852923c3
lpofredc <frederic.cloitre@lpo.fr>
2018-10-06 01:55:49 +0200

    first work on frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,15 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SightsComponent } from './sights/sights.component';
-import { MapComponent } from './sights/map/map.component';
-import { FormComponent } from './sights/form/form.component';
-import { ListComponent } from './sights/list/list.component';
-import { HomeComponent } from './home/home.component';
-import { SurveysComponent } from './surveys/surveys.component';
-import { RegisterComponent } from './register/register.component';
+import {AppComponent} from './app.component';
+import {SightsComponent} from './sights/sights.component';
+import {MapComponent} from './sights/map/map.component';
+import {FormComponent} from './sights/form/form.component';
+import {ListComponent} from './sights/list/list.component';
+import {HomeComponent} from './home/home.component';
+import {SurveysComponent} from './surveys/surveys.component';
+import {RegisterComponent} from './register/register.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {RouterModule} from '@angular/router';
+
 
 
 @NgModule({
@@ -25,9 +27,14 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'sights', component: SightsComponent},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- frontend/src/app/sights/sights.component.ts
+++ frontend/src/app/sights/sights.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import * as L from "leaflet";
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
+declare let $: any;
 
 @Component({
   selector: 'app-sights',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SightsComponent implements OnInit {
 
-  constructor() { }
+
+
+  constructor(private modalService: NgbModal) {}
+
+
+  modal(content) {
+    this.modalService.open(content, {});
+  }
 
   ngOnInit() {
+       // Déclaration de la carte avec les coordonnées du centre et le niveau de zoom.
+    const mysightmap = L.map('sightmap').setView([45, 5], 12);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'OpenStreetMap'
+    }).addTo(mysightmap);
+
+    const myIcon = L.icon({
+      iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+    });
+    var myMarker = null;
+
+    var myMarkerTitle = '<i class="fa fa-eye"></i> Partagez votre observation';
+    // var myMarkerContent = 'Texte du contenu du modal';
+
+
+    //
+    // myMarker.on({
+    //   click: function (e) {
+    //     $("#feature-title").html(myMarkerTitle);
+    //     // $("#feature-info").html(myMarkerContent);
+    //     $("#featureModal").modal("show");
+    //   }
+    // });
+    mysightmap.on('click', function <LeafletMouseEvent>(e) {
+      //var Coords = "Lat, Lon : " + e.latlng.lat.toFixed(3) + ", " + e.latlng.lng.toFixed(3);
+      var Coords = JSON.stringify({ type: "Point", coordinates: [e.latlng.lng, e.latlng.lat]});
+      if (myMarker !== null) {
+        mysightmap.removeLayer(myMarker);
+      }
+      myMarker = L.marker(e.latlng, {icon: myIcon}).addTo(mysightmap);
+      $("#feature-title").html(myMarkerTitle);
+      $("#feature-coords").html(Coords);
+      // $("#feature-info").html(myMarkerContent);
+      $("#featureModal").modal("show");
+    });
+
+
+
+    //
+    // var myMarkerTitle='Titre du modal';
+    // var myMarkerContent='Mon contenu';
+
+    // myMarker.on({
+    //   click: function (e) {
+    //     $("#feature-title").html(myMarkerTitle);
+    //     $("#feature-info").html(myMarkerContent);
+    //     $("#featureModal").modal("show");
+    //   }
+    // });
   }
 
 }
