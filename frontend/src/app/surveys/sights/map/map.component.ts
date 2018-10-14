db04e7e333f3ed16d374bed2c2a15d27dba95f0c
lpofredc <frederic.cloitre@lpo.fr>
2018-10-15 01:19:16 +0200

    update sight map component

--- frontend/src/app/surveys/sights/map/map.component.ts
+++ frontend/src/app/surveys/sights/map/map.component.ts
@@ -4,6 +4,7 @@ import {HttpClient} from '@angular/common/http';
 import * as L from 'leaflet';
 import {map} from 'rxjs/operators';
 import {AppConfig} from '../../../../conf/app.config';
+import { ThrowStmt } from '@angular/compiler';
 
 declare let $: any;
 
@@ -15,8 +16,10 @@ declare let $: any;
 
 export class SightsMapComponent implements OnInit {
   sightsGeoJson: any;
+  portalAreaGeoJson: any;
   coords: any;
   survey_id: any;
+  mysightmap: any;
 
   constructor(
     private http: HttpClient,
@@ -29,84 +32,107 @@ export class SightsMapComponent implements OnInit {
 
 
   ngOnInit() {
+    this.initMap();
     this.getSightsItems();
-  }
+    this.getPortalArea();
+  };
 
 
   getSightsItems(): void {
     this.restItemsServiceGetSightsItems().subscribe(sights => {
       this.sightsGeoJson = sights;
 
-      const mysightmap = L.map('sightmap').setView([45, 5], 12);
-
-      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'OpenStreetMap'
-      }).addTo(mysightmap);
-
-      const markerIcon = L.icon({
-        iconUrl:
-          'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
-      });
-
-      let myMarker = null;
-
-      const myMarkerTitle =
-        '<i class="fa fa-eye"></i> Partagez votre observation';
-
-      mysightmap.on('click', function <LeafletMouseEvent>(e) {
-        //var Coords = "Lat, Lon : " + e.latlng.lat.toFixed(3) + ", " + e.latlng.lng.toFixed(3);
-        let coords = JSON.stringify({
-          type: 'Point',
-          coordinates: [e.latlng.lng, e.latlng.lat]
-        });
-        this.coords = coords;
-        console.log(coords);
-        if (myMarker !== null) {
-          mysightmap.removeLayer(myMarker);
-        }
-        myMarker = L.marker(e.latlng, {icon: markerIcon}).addTo(mysightmap);
-        $('#feature-title').html(myMarkerTitle);
-        $('#feature-coords').html(coords);
-        // $("#feature-info").html(myMarkerContent);
-        $('#featureModal').modal('show');
-      });
-
       const geoSights = this.sightsGeoJson;
 
-      const geojsonMarkerOptions = {
-        radius: 5,
-        fillColor: '#1779ba',
-        color: '#ccc',
-        weight: 1,
-        opacity: 1,
-        fillOpacity: 0.8
-      };
+    const mysightmap = this.mysightmap;
 
-      function onEachFeature(feature, layer) {
-        let popupContent = "<b>" 
-        + feature.properties.common_name 
-        + "</b> (<i>"
-        + feature.properties.sci_name
-        +"</i>)</br>le " + feature.properties.date;
-        if (feature.properties && feature.properties.popupContent) {
-          popupContent += feature.properties.popupContent;
-        }
-        layer.bindPopup(popupContent);
+    const geojsonMarkerOptions = {
+      radius: 5,
+      fillColor: '#1779ba',
+      color: '#ccc',
+      weight: 1,
+      opacity: 1,
+      fillOpacity: 0.8
+    };
+
+    function onEachFeature(feature, layer) {
+      let popupContent = "<b>" 
+      + feature.properties.common_name 
+      + "</b> (<i>"
+      + feature.properties.sci_name
+      +"</i>)</br>le " + feature.properties.date;
+      if (feature.properties && feature.properties.popupContent) {
+        popupContent += feature.properties.popupContent;
       }
+      layer.bindPopup(popupContent);
+    }
 
-      function pointToLayer(feature, latlng) {
-        return L.circleMarker(latlng, geojsonMarkerOptions);
-      }
+    function pointToLayer(feature, latlng) {
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
 
-      console.log('SIGHTS :', geoSights);
-      L.geoJSON(geoSights,
-        {
-          onEachFeature: onEachFeature,
-          pointToLayer: pointToLayer,).addTo(mysightmap);
+    console.log('SIGHTS :', geoSights);
+    L.geoJSON(geoSights,
+      {
+        onEachFeature: onEachFeature,
+        pointToLayer: pointToLayer
+      }).addTo(mysightmap);
     });
+  }
+
+  getPortalArea(): void {
+    this.restItemsServiceGetPortalArea().subscribe(portalarea => {
+    this.portalAreaGeoJson = portalarea;
+    const mysightmap = this.mysightmap;
+    const portalArea = this.portalAreaGeoJson
+    L.geoJSON(portalArea).addTo(mysightmap);
+    });
+  }
+
+  initMap() {
+    const mysightmap = L.map('sightmap').setView([45, 5], 12);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'OpenStreetMap'
+    }).addTo(mysightmap);
+
+    const markerIcon = L.icon({
+      iconUrl:
+        'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.2.0/images/marker-icon.png'
+    });
+
+    let myMarker = null;
+
+    const myMarkerTitle =
+      '<i class="fa fa-eye"></i> Partagez votre observation';
+
+    mysightmap.on('click', function <LeafletMouseEvent>(e) {
+      //var Coords = "Lat, Lon : " + e.latlng.lat.toFixed(3) + ", " + e.latlng.lng.toFixed(3);
+      let coords = JSON.stringify({
+        type: 'Point',
+        coordinates: [e.latlng.lng, e.latlng.lat]
+      });
+      this.coords = coords;
+      console.log(coords);
+      if (myMarker !== null) {
+        mysightmap.removeLayer(myMarker);
+      }
+      myMarker = L.marker(e.latlng, {icon: markerIcon}).addTo(mysightmap);
+      $('#feature-title').html(myMarkerTitle);
+      $('#feature-coords').html(coords);
+      // $("#feature-info").html(myMarkerContent);
+      $('#featureModal').modal('show');
+    });
+
+    this.mysightmap = mysightmap;
+
   }
 
   restItemsServiceGetSightsItems() {
     return this.http.get(`${AppConfig.API_ENDPOINT}/sights`).pipe(map(data => data));
+  }
+
+  restItemsServiceGetPortalArea() {
+    return this.http.get(`${AppConfig.API_ENDPOINT}/portalarea`).pipe(map(data => data));
   }
 }
