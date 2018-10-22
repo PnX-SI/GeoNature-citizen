d6552012fdd9b0bbd6ea86dead0c5f4723d0a3e3
fred.lpo07 <frederic.cloitre@lpo.fr>
2018-10-22 10:01:23 +0200

    use app.config.ts

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,25 +1,25 @@
 import { Injectable } from "@angular/core";
 import { Headers, Http } from "@angular/http";
 import { LoginUser, RegisterUser } from "./models";
+import { AppConfig } from "../../conf/app.config";
 
 @Injectable()
 export class AuthService {
-  private BASE_URL: string = "http://localhost:5001/api";
   private headers: Headers = new Headers({
     "Content-Type": "application/json"
   });
   constructor(private http: Http) {}
   login(user: LoginUser): Promise<any> {
-    let url = `${this.BASE_URL}/login`;
+    let url = `${AppConfig.API_ENDPOINT}/login`;
     return this.http.post(url, user, { headers: this.headers }).toPromise();
     console.log('LoginProcess');
   }
   register(user: RegisterUser): Promise<any> {
-    let url: string = `${this.BASE_URL}/registration`;
+    let url: string = `${AppConfig.API_ENDPOINT}/registration`;
     return this.http.post(url, user, { headers: this.headers }).toPromise();
   }
   logout(access_token): Promise<any> {
-    let url: string = `${this.BASE_URL}/logout`;
+    let url: string = `${AppConfig.API_ENDPOINT}/logout`;
     let headers: Headers = new Headers({
       "Content-Type": "application/json",
       Authorization: `Bearer ${access_token}`
@@ -27,7 +27,7 @@ export class AuthService {
     return this.http.post(url, { headers: headers }).toPromise();
   }
   ensureAuthenticated(access_token): Promise<any> {
-    let url: string = `${this.BASE_URL}/logged_user`;
+    let url: string = `${AppConfig.API_ENDPOINT}/logged_user`;
     let headers: Headers = new Headers({
       "Content-Type": "application/json",
       Authorization: `Bearer ${access_token}`
