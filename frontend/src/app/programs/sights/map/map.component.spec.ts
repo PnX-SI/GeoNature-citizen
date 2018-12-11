dbd0620ffa49c4d72ff82afefd6cf0c10a82077e
pat <patkap@users.noreply.github.com>
2018-12-11 01:12:28 +0100

    Update fe tests

--- frontend/src/app/programs/sights/map/map.component.spec.ts
+++ frontend/src/app/programs/sights/map/map.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { MapComponent } from './map.component';
+import { SightsMapComponent } from './map.component';
 
-describe('MapComponent', () => {
-  let component: MapComponent;
-  let fixture: ComponentFixture<MapComponent>;
+describe('SightsMapComponent', () => {
+  let component: SightsMapComponent;
+  let fixture: ComponentFixture<SightsMapComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MapComponent ]
+      declarations: [ SightsMapComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(MapComponent);
+    fixture = TestBed.createComponent(SightsMapComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
