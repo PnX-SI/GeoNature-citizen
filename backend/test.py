ab1b61378c7e5a4e25c8b31d978259922207d3b8
pat <patkap@users.noreply.github.com>
2018-12-14 10:32:42 +0100

    sync.

--- backend/test.py
+++ backend/test.py
@@ -4,7 +4,7 @@ import unittest
 import requests
 
 from gncitizen.utils.env import load_config
-# from server import get_app
+from server import get_app
 
 APP_CONF = load_config()
 access_token = None
@@ -20,13 +20,6 @@ pwd = 'testpwd'  # noqa: S105
 name = 'tester'
 surname = 'testersurname'
 email = 'tester@test.com'
-
-TEST_POST_SIGHTS = json.dumps({
-    'cd_nom': 3582,
-    'obs_txt': 'Tada',
-    'count': 1,
-    'geometry': {"type": "Point", "coordinates": [5, 45]}
-})
 
 
 def auth():
@@ -74,10 +67,35 @@ class TestAuthFlaskApiUsingRequests(unittest.TestCase):
         self.assertEqual(response.json(), {"msg": "Successfully logged out"})
 
 
-class TestFlaskApiUsingRequests(unittest.TestCase):
+class SightsTestCase(unittest.TestCase):
+    def setUp(self):
+        """Define test variables and initialize app."""
+        self.app = get_app(load_config())
+        self.client = self.app.test_client
+        self.sights_post_data = {
+            'cd_nom': 3582,
+            'obs_txt': 'Tada',
+            'count': 1,
+            'geometry': {"type": "Point", "coordinates": [5, 45]}
+        }
+
+    def login_user(self, username=user, password=pwd):
+        # user_data = {
+        #     'username': username,
+        #     'password': password
+        # }
+        return self.client().post(mainUrl + 'login', data=auth())
 
     def test_get_sights(self):
         response = getrequest("sights")
+        # result = self.login_user()
+        # print('login resp:', result.data.decode())
+        # access_token = json.loads(result.data.decode())['access_token']
+        # response = self.client().post(
+        #     '/api/sights/',
+        #     headers=dict(Authorization="Bearer " + access_token),
+        #     data=self.sights_post_data)
+
         self.assertEqual(response.status_code, 200)
         data = response.json()
         self.assertEqual(data['type'], "FeatureCollection")
