a2b1783ba173062e41cf8babab37609339c0c71f
lpofredc <frederic.cloitre@lpo.fr>
2018-12-18 22:01:10 +0100

    Observations filtered by programs in frontend

--- frontend/src/app/programs/sights/list/list.component.ts
+++ frontend/src/app/programs/sights/list/list.component.ts
@@ -1,19 +1,18 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
-import {map} from 'rxjs/operators';
-import {AppConfig} from '../../../../conf/app.config';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { HttpClient } from "@angular/common/http";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { map } from "rxjs/operators";
+import { AppConfig } from "../../../../conf/app.config";
 
 @Component({
-  selector: 'app-sight-list',
-  templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  selector: "app-sight-list",
+  templateUrl: "./list.component.html",
+  styleUrls: ["./list.component.css"]
 })
 export class SightsListComponent implements OnInit {
-  survey_id: any;
+  program_id: any;
   sightsFeatures: any;
-
 
   constructor(
     // private rest: RestService,
@@ -22,22 +21,23 @@ export class SightsListComponent implements OnInit {
     private modalService: NgbModal
   ) {
     this.route.params.subscribe(params => {
-      this.survey_id = params['id'];
+      this.program_id = params[`id`];
     });
   }
 
   ngOnInit() {
-    this.getSightsFeatures();
+    this.getSightsFeatures(this.program_id);
   }
 
-  getSightsFeatures(): void {
-    this.restItemsServiceGetSightsItems().subscribe(sights => {
-      this.sightsFeatures = sights['features'];
+  getSightsFeatures(program_id): void {
+    this.restItemsServiceGetSightsItems(program_id).subscribe(sights => {
+      this.sightsFeatures = sights[`features`];
     });
   }
 
-  restItemsServiceGetSightsItems() {
-    return this.http.get(`${AppConfig.API_ENDPOINT}/sights`).pipe(map(data => data));
+  restItemsServiceGetSightsItems(program_id) {
+    return this.http
+      .get(`${AppConfig.API_ENDPOINT}/programs/` + program_id + `/observations`)
+      .pipe(map(data => data));
   }
-
 }
