9d2880b874d963aefbbfcd3f70d722dea902e5c5
patkap <patkap@users.noreply.github.com>
2019-02-04 19:25:15 +0100

    [wip] conditional routing

--- frontend/src/app/programs/default-program.guard.ts
+++ frontend/src/app/programs/default-program.guard.ts
@@ -1,46 +1,54 @@
-import { Injectable } from '@angular/core'
+import { Injectable } from "@angular/core";
 import {
   CanActivate,
   CanActivateChild,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
   Router
-} from '@angular/router'
-import { Observable, of } from 'rxjs'
-import { map, catchError } from 'rxjs/operators'
+} from "@angular/router";
+import { Observable, of } from "rxjs";
+import { map, catchError } from "rxjs/operators";
 
-import { GncProgramsService } from '../api/gnc-programs.service'
-import { Program } from './programs.models'
+import { GncProgramsService } from "../api/gnc-programs.service";
+import { Program } from "./programs.models";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UniqueProgramGuard implements CanActivate, CanActivateChild {
-  constructor(private programService: GncProgramsService, private router: Router) {}
+  constructor(
+    private programService: GncProgramsService,
+    private router: Router
+  ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return this.programService.getAllPrograms().pipe(  // FIXME: leverage resolve
+    return this.programService.getAllPrograms().pipe(
+      // FIXME: leverage resolve
       map((p: Program[]) => {
-        const count = (p) ? p.length : 0
-        const programs = (p) ? p : undefined
-        console.debug(`UniqueProgramGuard program count: ${count}`, programs)
+        const count = p ? p.length : 0;
+        const programs = p ? p : undefined;
+        console.debug(`UniqueProgramGuard program count: ${count}`, programs);
         if (count === 1) {
-          this.router.navigate(['programs', programs[0].id_program, 'observations'])
-          return false
+          this.router.navigate([
+            "programs",
+            programs[0].id_program,
+            "observations"
+          ]);
+          return false;
         }
-        return true
+        return true;
       }),
       catchError(_e => of(true))
-    )
+    );
   }
 
   canActivateChild(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    return this.canActivate(route, state)
+    return this.canActivate(route, state);
   }
 }
