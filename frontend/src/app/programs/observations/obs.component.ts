2feb252c059f437791b3149d461d377ef85c08ef
pat <patkap@users.noreply.github.com>
2019-02-08 07:01:37 +0100

    Fix program desc embedded html display using domsanitizer. Automatic linting

--- frontend/src/app/programs/observations/obs.component.ts
+++ frontend/src/app/programs/observations/obs.component.ts
@@ -1,38 +1,43 @@
-import {
-  Component,
-  OnInit,
-  ViewEncapsulation,
-} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { DomSanitizer } from "@angular/platform-browser";
 
-import { ModalFlowService } from './modalflow/modalflow.service'
-import { ActivatedRoute } from '@angular/router';
-import { Program } from '../programs.models';
+import { ModalFlowService } from "./modalflow/modalflow.service";
+import { ActivatedRoute } from "@angular/router";
+import { Program } from "../programs.models";
 
 @Component({
-  selector: 'app-observations',
-  templateUrl: './obs.component.html',
-  styleUrls: ['./obs.component.css', '../../home/home.component.css'],
-  encapsulation: ViewEncapsulation.None,
+  selector: "app-observations",
+  templateUrl: "./obs.component.html",
+  styleUrls: ["./obs.component.css", "../../home/home.component.css"],
+  encapsulation: ViewEncapsulation.None
 })
 export class ObsComponent implements OnInit {
-  title = 'Observations';
+  title = "Observations";
   survey_id: any;
   coords: any;
-  programs: Program[]
-  program: Program
+  programs: Program[];
+  program: Program;
+  programShortDescription: any;
+  programLongDescription: any;
 
   constructor(
     private route: ActivatedRoute,
-    public flowService: ModalFlowService,
+    protected domSanitizer: DomSanitizer,
+    public flowService: ModalFlowService
   ) {
-    this.route.params.subscribe(params => this.survey_id = params['id'])
+    this.route.params.subscribe(params => (this.survey_id = params["id"]));
   }
 
   ngOnInit() {
-    this.route.data
-      .subscribe((data: { programs: Program[] }) => {
-        this.programs = data.programs
-        this.program = this.programs.find(p => p.id_program == this.survey_id)
-    })
+    this.route.data.subscribe((data: { programs: Program[] }) => {
+      this.programs = data.programs;
+      this.program = this.programs.find(p => p.id_program == this.survey_id);
+      this.programShortDescription = this.domSanitizer.bypassSecurityTrustHtml(
+        this.program.short_desc
+      );
+      this.programLongDescription = this.domSanitizer.bypassSecurityTrustHtml(
+        this.program.long_desc
+      );
+    });
   }
 }
