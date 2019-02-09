248cce3cbbfc564cd19fbbef118bc40be8067f2a
patkap <patkap@users.noreply.github.com>
2019-02-09 11:45:18 +0100

    ready for merge.

--- frontend/src/app/app.server.module.ts
+++ frontend/src/app/app.server.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { NoopAnimationsModule } from '@angular/platform-browser/animations'
+// import { NoopAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -9,7 +9,7 @@ import { AppComponent } from './app.component';
   imports: [
     AppModule,
     ServerModule,
-    NoopAnimationsModule
+    // NoopAnimationsModule
   ],
   bootstrap: [AppComponent],
 })
