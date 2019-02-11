bf71adb2174a78ee2fd8ea1ca9fb36c30474febe
patkap <patkap@users.noreply.github.com>
2019-02-11 19:38:20 +0100

    [wip] emergency sync

--- frontend/src/app/programs/observations/list/list.component.ts
+++ frontend/src/app/programs/observations/list/list.component.ts
@@ -1,55 +1,23 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { HttpClient } from "@angular/common/http";
-import { FormGroup } from "@angular/forms";
-import { map } from "rxjs/operators";
+import { Component, OnChanges, Input } from "@angular/core";
+// import { FormGroup } from "@angular/forms";
 
-import { AppConfig } from "../../../../conf/app.config";
+import { FeatureCollection, Feature } from "geojson";
 
 @Component({
   selector: "app-obs-list",
   templateUrl: "./list.component.html",
   styleUrls: ["./list.component.css"]
 })
-export class ObsListComponent implements OnInit {
-  program_id: any;
-  observationsFeatures: any;
-  // @Input() formGroup: FormGroup
-  surveySpecies: any
+export class ObsListComponent implements OnChanges {
+  @Input("observations") obs: FeatureCollection;
+  @Input("taxa") surveySpecies: any[];
+  observations: Feature[] = [];
+  program_id: number;
+  taxa: any[];
 
-  constructor(
-    private http: HttpClient,
-    private route: ActivatedRoute,
-  ) {
-    this.route.params.subscribe(params => {
-      this.program_id = params[`id`];
-    });
-  }
-
-  ngOnInit() {
-    this.getObsFeatures(this.program_id);
-  }
-
-  getObsFeatures(program_id=1): void {
-    this.restItemsServiceGetObsItems(program_id).subscribe(observations => {
-      this.observationsFeatures = observations[`features`];
-    });
-  }
-
-  restItemsServiceGetObsItems(program_id=1) {
-    return this.http
-      .get(`${AppConfig.API_ENDPOINT}/programs/` + program_id + `/observations`)
-      .pipe(map(data => data));
-  }
-
-  restItemsServiceGetSurveySpeciesItems(taxlist) {
-    return this.http
-      .get(`${AppConfig.API_ENDPOINT}/taxonomy/lists/${taxlist}/species`)
-      .pipe(map(data => data));
-  }
-
-  getSurveySpeciesItems(taxlist): void {
-    this.restItemsServiceGetSurveySpeciesItems(taxlist)
-        .subscribe(species => this.surveySpecies = species);
+  ngOnChanges() {
+    if (this.obs) {
+      this.observations = this.obs["features"];
+    }
   }
 }
