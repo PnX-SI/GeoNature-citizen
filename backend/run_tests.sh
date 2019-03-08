e00d80eef7b00a0e2f699ffad1d526f876d9c0e2
Jules Olleon <jolleon@gmail.com>
2019-03-08 11:12:39 +0100

    add test user creation instruction

--- backend/run_tests.sh
+++ backend/run_tests.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Heads up! Test user needs to be created by hand the first time before
+# running tests. C.f. 'common.py' for SQL query to run.
+
 if [ -z $1 ]; then
     python -m unittest -v tests/test_*
 else
