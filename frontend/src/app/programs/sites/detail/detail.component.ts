b9c17e4ad6f59f87326a81303355028fe1a32889
Jules Olleon <jolleon@gmail.com>
2019-03-15 20:46:00 +0100

    full details page

--- frontend/src/app/programs/sites/detail/detail.component.ts
+++ frontend/src/app/programs/sites/detail/detail.component.ts
@@ -2,7 +2,9 @@ import {Component, AfterViewInit} from '@angular/core';
 import {GncProgramsService} from "../../../api/gnc-programs.service";
 import {ActivatedRoute} from "@angular/router";
 import * as L from "leaflet";
+import MaresJson from '../../../../../../config/custom/form/mares.json';
 
+declare let $: any;
 
 export const obsFormMarkerIcon = L.icon({
   iconUrl: "../../../../assets/pointer-blue2.png", // TODO: Asset path should be normalized, conf ?
@@ -17,6 +19,15 @@ export const obsFormMarkerIcon = L.icon({
 export class SiteDetailComponent implements AfterViewInit {
   site_id: any;
   site: any;
+  attributes = [];
+  photos = [{
+    url: "../../assets/Azure-Commun-019.JPG",
+    description: "Photo - Anonyme"
+  }, {
+    url: "../../assets/faune-mercantour.jpg",
+    description: "des bébêtes - Anonyme"
+  }
+  ];
 
   constructor(
     private route: ActivatedRoute,
@@ -31,9 +42,11 @@ export class SiteDetailComponent implements AfterViewInit {
     this.programService
       .getSiteDetails(this.site_id)
       .subscribe(sites => {
+
         console.log(sites);
         this.site = sites['features'][0];
 
+        // setup map
         const formMap = L.map("formMap");
         L.tileLayer("//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
           attribution: "OpenStreetMap"
@@ -43,9 +56,17 @@ export class SiteDetailComponent implements AfterViewInit {
         let latLng = L.latLng(coord[1], coord[0]);
         formMap.setView(latLng, 13);
 
-        L.marker(latLng, { icon: obsFormMarkerIcon }).addTo(
-              formMap
-            );
+        L.marker(latLng, { icon: obsFormMarkerIcon })
+          .addTo(formMap);
+
+        // prepare data
+        let data = this.site.properties.last_visit.json_data;
+        let schema = MaresJson.schema.properties;
+        for (const k in data) {
+          let v = data[k];
+          this.attributes.push({name: schema[k].title, value: v.toString()})
+        }
       });
   }
+
 }
