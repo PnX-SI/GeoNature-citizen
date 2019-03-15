8d9991ee5461a7757049b2c989d660a38d7707d5
Jules Olleon <jolleon@gmail.com>
2019-03-15 15:38:15 +0100

    get site details from backend

--- frontend/src/app/programs/sites/detail/detail.component.ts
+++ frontend/src/app/programs/sites/detail/detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { SiteModalFlowService } from "../modalflow/modalflow.service";
+import {GncProgramsService} from "../../../api/gnc-programs.service";
+import {ActivatedRoute} from "@angular/router";
+import { Feature } from "geojson";
 
 @Component({
   selector: 'app-site-detail',
@@ -7,10 +10,27 @@ import { SiteModalFlowService } from "../modalflow/modalflow.service";
   styleUrls: ['./detail.component.css']
 })
 export class SiteDetailComponent implements OnInit {
+  site_id: any;
+  site: Feature;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private programService: GncProgramsService,
+  ) {
+    this.route.params.subscribe(params => {
+      this.site_id = params["id"];
+    });
+  }
 
   ngOnInit() {
+    console.debug("getting site");
+    this.programService
+      .getSiteDetails(this.site_id)
+      .subscribe(sites => {
+        console.log(sites);
+        this.site = sites['features'][0];
+        console.log(this.site);
+      });
   }
 
 }
