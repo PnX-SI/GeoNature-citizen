3b7cd4db5c2b218a25bc5134902d2948280a123f
patkap <patkap@users.noreply.github.com>
2019-03-15 18:21:03 +0100

    [wip] sync

--- frontend/src/app/programs/observations/modalflow/steps/committed/committed.component.ts
+++ frontend/src/app/programs/observations/modalflow/steps/committed/committed.component.ts
@@ -1,8 +1,17 @@
-import { Component, Input, ViewChild, ViewEncapsulation } from "@angular/core";
+import {
+  Component,
+  Input,
+  ViewChild,
+  ViewEncapsulation,
+  ElementRef,
+  HostListener
+} from "@angular/core";
 
 import { IFlowComponent } from "../../flow/flow";
 import { ObsFormComponent } from "../../../form/form.component";
 import { ObservationFeature } from "../../../observation.model";
+
+interface NewObservationEvent extends CustomEvent {}
 
 @Component({
   templateUrl: "./committed.component.html",
@@ -12,12 +21,26 @@ import { ObservationFeature } from "../../../observation.model";
 export class CommittedComponent implements IFlowComponent {
   @Input() data: any;
   @ViewChild(ObsFormComponent) form: ObsFormComponent;
+  obsSubmit: ElementRef;
 
   onNewObservation(observation: ObservationFeature) {
     this.data.obs = observation;
     console.debug("committed action > onNewObservation data:", this.data);
+    const event: CustomEvent = new CustomEvent("NewObservationEvent", {
+      bubbles: true,
+      cancelable: true,
+      detail: observation
+    });
+    this.obsSubmit.nativeElement.dispatchEvent(event);
     this.data.next(this.data);
   }
+
+  // @HostListener("NewObservationEvent", ["$event"])
+  // public NewObservationEventHandler(e: CustomEvent) {
+  //   const customEvent: NewObservationEvent =
+  //     e instanceof NewObservationEvent ? e : <NewObservationEvent>e.detail;
+  //   // ...
+  // }
 
   committed() {
     this.form.onFormSubmit();
