ac7f25566a416d400cd56b994ddd9f893cd470d8
Jules Olleon <jolleon@gmail.com>
2019-03-15 17:11:29 +0100

    add map on details page

--- frontend/src/app/programs/sites/detail/detail.component.ts
+++ frontend/src/app/programs/sites/detail/detail.component.ts
@@ -1,17 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-import { SiteModalFlowService } from "../modalflow/modalflow.service";
+import {Component, AfterViewInit} from '@angular/core';
 import {GncProgramsService} from "../../../api/gnc-programs.service";
 import {ActivatedRoute} from "@angular/router";
-import { Feature } from "geojson";
+import * as L from "leaflet";
+
+
+export const obsFormMarkerIcon = L.icon({
+  iconUrl: "../../../../assets/pointer-blue2.png", // TODO: Asset path should be normalized, conf ?
+  iconAnchor: [16, 42]
+});
 
 @Component({
   selector: 'app-site-detail',
   templateUrl: './detail.component.html',
   styleUrls: ['./detail.component.css']
 })
-export class SiteDetailComponent implements OnInit {
+export class SiteDetailComponent implements AfterViewInit {
   site_id: any;
-  site: Feature;
+  site: any;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,15 +27,25 @@ export class SiteDetailComponent implements OnInit {
     });
   }
 
-  ngOnInit() {
-    console.debug("getting site");
+  ngAfterViewInit() {
     this.programService
       .getSiteDetails(this.site_id)
       .subscribe(sites => {
         console.log(sites);
         this.site = sites['features'][0];
-        console.log(this.site);
+
+        const formMap = L.map("formMap");
+        L.tileLayer("//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+          attribution: "OpenStreetMap"
+        }).addTo(formMap);
+
+        let coord = this.site.geometry.coordinates;
+        let latLng = L.latLng(coord[1], coord[0]);
+        formMap.setView(latLng, 13);
+
+        L.marker(latLng, { icon: obsFormMarkerIcon }).addTo(
+              formMap
+            );
       });
   }
-
 }
