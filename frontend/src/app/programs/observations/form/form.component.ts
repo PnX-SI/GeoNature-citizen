29d96532f0c7b74726fc34cf30b20cb86b980462
patkap <patkap@users.noreply.github.com>
2019-04-12 12:38:11 +0200

    obsFrom: fix date validator. it seems Angular uses r.o.p. for validators composition, hence ngbDateMaxIsToday() should error if date is null.

--- frontend/src/app/programs/observations/form/form.component.ts
+++ frontend/src/app/programs/observations/form/form.component.ts
@@ -57,6 +57,7 @@ export function ngbDateMaxIsToday(): ValidatorFn {
   return (control: AbstractControl): { [key: string]: any } | null => {
     const today = new Date();
     const selected = NgbDate.from(control.value);
+    if (!selected) return { "Null date": true };
     const date_impl = new Date(selected.year, selected.month - 1, selected.day);
     return date_impl > today ? { "Parsed a date in the future": true } : null;
   };
