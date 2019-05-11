a453530853f7562157ead9cd743641a77b398d97
patkap <patkap@users.noreply.github.com>
2019-05-11 10:48:33 +0200

    pgsql seq reset test.

--- data/demo_data.sql
+++ data/demo_data.sql
@@ -475,21 +475,3 @@ select uuid_generate_v4()
 from d, g
 where d.id = g.id
 ;
-
-SELECT 'SELECT SETVAL(' ||
-       quote_literal(quote_ident(PGT.schemaname) || '.' || quote_ident(S.relname)) ||
-       ', COALESCE(MAX(' ||quote_ident(C.attname)|| '), 1) ) FROM ' ||
-       quote_ident(PGT.schemaname)|| '.'||quote_ident(T.relname)|| ';'
-FROM pg_class AS S,
-     pg_depend AS D,
-     pg_class AS T,
-     pg_attribute AS C,
-     pg_tables AS PGT
-WHERE S.relkind = 'S'
-    AND S.oid = D.objid
-    AND D.refobjid = T.oid
-    AND D.refobjid = C.attrelid
-    AND D.refobjsubid = C.attnum
-    AND T.relname = PGT.tablename
-    AND PGT.schemaname LIKE 'gnc_%'
-ORDER BY S.relname;
