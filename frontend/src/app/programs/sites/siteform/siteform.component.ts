985f53b1154dc969a228182391d64d24c9b2ef37
Quentin Jouet <quentin.jouet@gmail.com>
2019-05-29 16:47:41 +0200

    Site form map fullscreen option and zoom constraints (from observations)

--- frontend/src/app/programs/sites/siteform/siteform.component.ts
+++ frontend/src/app/programs/sites/siteform/siteform.component.ts
@@ -3,7 +3,8 @@ import {
   ViewEncapsulation,
   AfterViewInit,
   ViewChild,
-  ElementRef
+  ElementRef,
+  Input
 } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { ActivatedRoute } from "@angular/router";
@@ -15,10 +16,21 @@ import { NgbDate } from "@ng-bootstrap/ng-bootstrap";
 import { Position, Point } from "geojson";
 import * as L from "leaflet";
 import { LeafletMouseEvent } from "leaflet";
+import "leaflet-fullscreen/dist/Leaflet.fullscreen";
+import "leaflet-gesture-handling";
 
 import { AppConfig } from "../../../../conf/app.config";
+import { MAP_CONFIG } from "../../../../conf/map.config";
 
 declare let $: any;
+
+const PROGRAM_AREA_STYLE = {
+  fillColor: "transparent",
+  weight: 2,
+  opacity: 0.8,
+  color: "red",
+  dashArray: "4"
+};
 
 // TODO: migrate to conf
 export const taxonListThreshold = 10;
@@ -37,16 +49,20 @@ export const myMarkerTitle =
 })
 export class SiteFormComponent implements AfterViewInit {
   private readonly URL = AppConfig.API_ENDPOINT;
+  @Input("coords") coords: L.Point;
   @ViewChild("photo") photo: ElementRef;
   program: any;
   program_id: any;
-  formMap: any;
+  formMap: L.Map;
   siteForm = new FormGroup({
     name: new FormControl("", Validators.required),
     geometry: new FormControl("", Validators.required),
     id_program: new FormControl(this.program_id),
     site_type: new FormControl("mare") // TODO : get site type
   });
+  MAP_CONFIG = MAP_CONFIG;
+  hasZoomAlert: boolean;
+  zoomAlertTimeout: any;
 
   constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
@@ -57,21 +73,45 @@ export class SiteFormComponent implements AfterViewInit {
       .subscribe(result => {
         this.program = result;
 
-        const formMap = L.map("formMap");
+        // build map control
+        const formMap = L.map("formMap", { gestureHandling: true });
+        this.formMap = formMap;
 
         L.tileLayer("//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
           attribution: "OpenStreetMap"
         }).addTo(formMap);
 
+        L.control["fullscreen"]({
+          position: "topright",
+          title: {
+            false: "View Fullscreen",
+            true: "Exit Fullscreen"
+          }
+        }).addTo(formMap);
+
+        let ZoomViewer = L.Control.extend({
+          onAdd: () => {
+            let container = L.DomUtil.create("div");
+            let gauge = L.DomUtil.create("div");
+            container.style.width = "200px";
+            container.style.background = "rgba(255,255,255,0.5)";
+            container.style.textAlign = "left";
+            container.className = "mb-0";
+            formMap.on("zoomstart zoom zoomend", function(_e) {
+              gauge.innerHTML = "Zoom level: " + formMap.getZoom();
+            });
+            container.appendChild(gauge);
+
+            return container;
+          }
+        });
+        let zv = new ZoomViewer();
+        zv.addTo(formMap);
+        zv.setPosition("bottomleft");
+
         const programArea = L.geoJSON(this.program, {
           style: function(_feature) {
-            return {
-              fillColor: "transparent",
-              weight: 2,
-              opacity: 0.8,
-              color: "red",
-              dashArray: "4"
-            };
+            return PROGRAM_AREA_STYLE;
           }
         }).addTo(formMap);
 
@@ -79,31 +119,52 @@ export class SiteFormComponent implements AfterViewInit {
         formMap.fitBounds(maxBounds);
         formMap.setMaxBounds(maxBounds);
 
+        // Set initial observation marker from main map if already spotted
         let myMarker = null;
+        if (this.coords) {
+          this.siteForm.patchValue({ geometry: this.coords });
 
-        formMap.on("click", <LeafletMouseEvent>(e) => {
-          let coords = <Point>{
-            type: "Point",
-            coordinates: <Position>[e.latlng.lng, e.latlng.lat]
-          };
-          this.siteForm.patchValue({ geometry: coords });
-          // TODO: this.siteForm.patchValue({ municipality: municipality });
-          console.debug(coords);
+          myMarker = L.marker([this.coords.y, this.coords.x], {
+            icon: siteFormMarkerIcon
+          }).addTo(formMap);
+        }
 
-          if (myMarker !== null) {
-            formMap.removeLayer(myMarker);
+        // Update marker on click event
+        formMap.on("click", (e: LeafletMouseEvent) => {
+          let z = formMap.getZoom();
+
+          if (z < MAP_CONFIG.ZOOM_LEVEL_RELEVE) {
+            // this.hasZoomAlert = true;
+            console.debug("ZOOM ALERT", formMap);
+            L.DomUtil.addClass(
+              formMap.getContainer(),
+              "observation-zoom-statement-warning"
+            );
+            if (this.zoomAlertTimeout) {
+              clearTimeout(this.zoomAlertTimeout);
+            }
+            this.zoomAlertTimeout = setTimeout(() => {
+              L.DomUtil.removeClass(
+                formMap.getContainer(),
+                "observation-zoom-statement-warning"
+              );
+              console.debug("Deactivating overlay", formMap);
+            }, 2000);
+            return;
           }
-
           // PROBLEM: if program area is a concave polygon: one can still put a marker in the cavities.
           // POSSIBLE SOLUTION: See ray casting algorithm for inspiration at https://stackoverflow.com/questions/31790344/determine-if-a-point-reside-inside-a-leaflet-polygon
           if (maxBounds.contains([e.latlng.lat, e.latlng.lng])) {
+            if (myMarker) {
+              // TODO: update marker coods inplace.
+              // Implement draggable marker
+              formMap.removeLayer(myMarker);
+            }
             myMarker = L.marker(e.latlng, { icon: siteFormMarkerIcon }).addTo(
               formMap
             );
-            $("#feature-title").html(myMarkerTitle);
-            $("#feature-coords").html(coords);
-            // $("#feature-info").html(myMarkerContent);
-            $("#featureModal").modal("show");
+            this.coords = L.point(e.latlng.lng, e.latlng.lat);
+            this.siteForm.patchValue({ geometry: this.coords });
           }
         });
       });
