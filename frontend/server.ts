c661619a45c00abcf9c24040959bb0327654a4c7
patkap <patkap@users.noreply.github.com>
2019-06-03 14:30:11 +0200

    misc ssr fixes. thx @20cents !

--- frontend/server.ts
+++ frontend/server.ts
@@ -77,14 +77,16 @@ app.get("*", (req, res) => {
   //   res.redirect(301, "/citizen/fr/home");
   // }
   const matches = req.url.match(/^\/([a-z]{2}(?:-[A-Z]{2})?)\//);
-  //check if the requested url has a correct format '/locale' and matches any of the supportedLocales
+  // check if the requested url has a correct format '/locale' and matches any of the supportedLocales
   const locale =
     matches && supportedLocales.indexOf(matches[1]) !== -1
       ? matches[1]
       : defaultLocale;
 
-  console.log("req.url:", req.url);
-  res.render(`${locale}/index`, { req });
+  res.render(`${locale}/index`, {
+        req: req,
+    url: req.url.replace(`${locale}/`, "")
+  });
 
   // res.render("index", { req });
 });
