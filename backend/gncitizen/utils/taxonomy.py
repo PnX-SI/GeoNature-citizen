1e27f08c4b9c9ea0d2ba6bb04e84403e052e4d4f
patkap <patkap@users.noreply.github.com>
2019-06-13 20:48:04 +0200

    taxo typing sync

--- backend/gncitizen/utils/taxonomy.py
+++ backend/gncitizen/utils/taxonomy.py
@@ -3,7 +3,7 @@
 
 """A module to manage taxonomy"""
 
-from typing import Dict, List
+from typing import Dict, List, Union
 from functools import lru_cache
 from flask import current_app
 
@@ -11,6 +11,7 @@ if current_app.config.get("API_TAXHUB") is None:
     from gncitizen.core.taxonomy.models import Taxref
 else:
     import requests
+    from requests.models import Response
 
     TAXHUB_API = (
         current_app.config["API_TAXHUB"] + "/"
@@ -18,26 +19,32 @@ else:
         else current_app.config["API_TAXHUB"]
     )
 
+Taxon = Dict[str, Union[str, Dict[str, str], List[Dict]]]
+
 
 def taxhub_rest_get_taxon_list(taxhub_list_id: int) -> Dict:
-    payload = {"existing": "true", "order": "asc", "orderby": "taxref.nom_complet"}
-    req = requests.get(
+    payload = {"existing": "true", "order": "asc",
+               "orderby": "taxref.nom_complet"}
+    res: Response = requests.get(
         "{}biblistes/taxons/{}".format(TAXHUB_API, taxhub_list_id),
         params=payload,
         timeout=1,
     )
-    req.raise_for_status()
-    return req.json()
+    res.raise_for_status()
+    return res.json()
 
 
-def taxhub_rest_get_taxon(taxhub_id: int) -> Dict[List[str], List[str]]:
-    req = requests.get("{}bibnoms/{}".format(TAXHUB_API, taxhub_id), timeout=1)
-    req.raise_for_status()
-    return req.json()
+def taxhub_rest_get_taxon(taxhub_id: int) -> Taxon:
+    if not taxhub_id:
+        raise ValueError("Null value for taxhub taxon id")
+    res: Response = requests.get(
+        "{}bibnoms/{}".format(TAXHUB_API, taxhub_id), timeout=1)
+    res.raise_for_status()
+    return res.json()
 
 
 @lru_cache()
-def mkTaxonRepository(taxhub_list_id: int) -> List:
+def mkTaxonRepository(taxhub_list_id: int) -> List[Taxon]:
     taxa = taxhub_rest_get_taxon_list(taxhub_list_id)
     taxon_ids = [item["id_nom"] for item in taxa.get("items")]
     return [taxhub_rest_get_taxon(taxon_id) for taxon_id in taxon_ids]
