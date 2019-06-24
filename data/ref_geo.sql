f0044495ecf46a3049c5d6cb2e19035c13bb3e16
lpofredc <frederic.cloitre@lpo.fr>
2019-06-25 01:04:58 +0200

    ref_geo scripts to populate municipalities

--- data/ref_geo.sql
+++ data/ref_geo.sql
@@ -6,6 +6,7 @@ set search_path = ref_geo, pg_catalog
 insert into ref_geo.bib_areas_types(id_type, type_name, type_code, type_desc, ref_name, ref_version, num_version)
 values (25, 'Communes', 'COM', 'Type commune', 'IGN admin_express', '2017', null)
 
+truncate table l_areas;
 
 /* Insertion des géométries dans l_areas */
 insert into l_areas (id_type, area_code, area_name, geom)
