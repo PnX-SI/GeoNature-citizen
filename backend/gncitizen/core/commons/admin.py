b41325463eb5747931355087ba801a446fd9dad5
lpofredc <frederic.cloitre@lpo.fr>
2019-09-04 23:10:24 +0200

    Remove forgotten 'form_overrides' attribute

--- backend/gncitizen/core/commons/admin.py
+++ backend/gncitizen/core/commons/admin.py
@@ -63,7 +63,6 @@ def taxonomy_lists():
 
 class ProgramView(ModelView):
     form_base_class = SecureForm
-    form_overrides = dict(long_desc=CKEditorField)
     form_overrides = {'long_desc':CKEditorField, 'taxonomy_list':SelectField}
     form_args = {'taxonomy_list':{'choices':taxonomy_lists(), 'coerce':int}}
     create_template = 'edit.html'
