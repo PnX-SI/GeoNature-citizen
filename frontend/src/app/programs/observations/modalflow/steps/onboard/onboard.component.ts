645b7e468c85da405f7ece55d6453b5b8affff56
Jean-Baptiste Desbas <jb.desbas@gmail.com>
2019-10-23 15:44:57 +0200

    skip login if signup not allowed

--- frontend/src/app/programs/observations/modalflow/steps/onboard/onboard.component.ts
+++ frontend/src/app/programs/observations/modalflow/steps/onboard/onboard.component.ts
@@ -13,6 +13,7 @@ import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
 import { IFlowComponent } from "../../flow/flow";
 import { RegisterComponent } from "../../../../../auth/register/register.component";
 import { LoginComponent } from "../../../../../auth/login/login.component";
+import { AppConfig } from "../../../../../../conf/app.config";
 import { AuthService } from "../../../../../auth/auth.service";
 
 @Component({
@@ -38,6 +39,9 @@ export class OnboardComponent implements IFlowComponent, OnInit {
     this.authService.authorized$.subscribe(value => {
       if (value) {
         this.timeout = setTimeout(() => this.data.next(), 0);
+      }
+      if (!AppConfig.signup) { 
+        this.data.next(); 
       }
     });
   }
