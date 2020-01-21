422ad5a8b2c1adda8df5a10972627857c602e3a7
lpofredc <frederic.cloitre@lpo.fr>
2020-01-21 11:19:48 +0100

    improve activation send mail util

--- backend/gncitizen/utils/mail_check.py
+++ backend/gncitizen/utils/mail_check.py
@@ -14,33 +14,41 @@ def confirm_user_email(newuser):
     msg["From"] = current_app.config["CONFIRM_EMAIL"]["FROM"]
     msg["To"] = newuser.email
 
+    # Check URL_APPLICATION:
+    url_application = current_app.config["URL_APPLICATION"]
+    if url_application[-1]=='/':
+        url_application = url_application
+    else:
+        url_application = url_application+'/'
+    print('url_application', url_application)
+    activate_url = url_application + "confirmEmail/" + token
     # Record the MIME  text/html.
     msg_body = MIMEText(
         current_app.config["CONFIRM_EMAIL"]["HTML_TEMPLATE"].format(
-            activate_url=current_app.config["URL_APPLICATION"] +
-            "confirmEmail/"+token
+            activate_url = activate_url
         ),
         "html")
 
     msg.attach(msg_body)
 
     try:
-        with smtplib.SMTP_SSL(
-            current_app.config["MAIL"]["MAIL_HOST"],
-            int(current_app.config["MAIL"]["MAIL_PORT"]),
-        ) as server:
-            server.ehlo()
-            if current_app.config["MAIL"]["MAIL_STARTTLS"]:
-                server.starttls()
-            server.login(
-                str(current_app.config["MAIL"]["MAIL_AUTH_LOGIN"]),
-                str(current_app.config["MAIL"]["MAIL_AUTH_PASSWD"]),
+        if current_app.config["MAIL"]["MAIL_USE_SSL"]:
+            server = smtplib.SMTP_SSL(current_app.config["MAIL"]["MAIL_HOST"],int(current_app.config["MAIL"]["MAIL_PORT"]))
+        else:
+            server = smtplib.SMTP(current_app.config["MAIL"]["MAIL_HOST"],int(current_app.config["MAIL"]["MAIL_PORT"]))
+
+        server.ehlo()
+        if current_app.config["MAIL"]["MAIL_STARTTLS"]:
+            server.starttls()
+        server.login(
+            str(current_app.config["MAIL"]["MAIL_AUTH_LOGIN"]),
+            str(current_app.config["MAIL"]["MAIL_AUTH_PASSWD"]),
+        )
+        server.sendmail(
+            current_app.config["CONFIRM_EMAIL"]["FROM"], newuser.email, msg.as_string(
             )
-            server.sendmail(
-                current_app.config["CONFIRM_EMAIL"]["FROM"], newuser.email, msg.as_string(
-                )
-            )
-            server.quit()
+        )
+        server.quit()
 
     except Exception as e:
         current_app.logger.warning(
