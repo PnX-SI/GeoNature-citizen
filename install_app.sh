df73cb4168f08021b58a632b0c1782a99bceb6d3
anthony <sel.anthony.pro@gmail.com>
2020-12-08 11:54:55 +0100

    update install app with all dependencies

--- install_app.sh
+++ install_app.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 DIR=$(pwd)
 
-#Installation de gunicorn
-apt update && apt install gunicorn
+#
 
 #création d'un fichier de configuration pour api/back
 if [ ! -f config/settings.ini ]; then
@@ -14,6 +13,115 @@ if [ ! -f config/settings.ini ]; then
 fi
 
 . config/settings.ini
+
+#Installation de python / gunicorn / supervisor + dépendances
+apt update && apt -y install python2.7 git gcc curl gunicorn python-setuptools sudo lsb-release apt-transport-https wget
+sudo -s apt -y install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev curl libbz2-dev
+sudo -s apt install apache2 python-dev libpq-dev libgeos-dev supervisor unzip -y
+
+RELEASE=$(cat /etc/os-release | grep VERSION_CODENAME |cut -d "=" -f2)
+echo $RELEASE
+wget https://people.debian.org/~paravoid/python-all/unofficial-python-all.asc
+sudo -s mv unofficial-python-all.asc /etc/apt/trusted.gpg.d/
+echo "deb http://people.debian.org/~paravoid/python-all $RELEASE main" | sudo tee /etc/apt/sources.list.d/python-all.list
+sudo -s apt update && sudo -s apt install -y python3.7
+update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 50
+sudo -s  apt-get clean
+
+echo `python3 --version`
+
+service supervisor start
+
+#Installation  de pip
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+
+CURRENT_DIR=$(pwd)
+
+#Installation de nvm / npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 14 --lts
+echo `npm -v`
+
+#Installation de taxhub
+if [ ! -d /home/synthese ]; then
+adduser --gecos "" --home /home/synthese synthese
+adduser synthese sudo
+adduser synthese root
+adduser synthese www-data
+fi
+cd /home/synthese
+python3 -m pip install virtualenv==20.0.1 --user
+wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.37.2/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install 14 --lts
+echo `npm -v`
+sudo a2enmod rewrite proxy proxy_http
+sudo apache2ctl restart
+sudo apt-get install postgresql postgresql-client postgresql-9.6-postgis-2.3 -y
+PG_VERSION="9.6"
+sudo adduser postgres sudo
+service postgresql start
+sudo -n -u postgres -s psql -c "CREATE USER $pg_user WITH PASSWORD '$user_pg_pass';"
+sudo -n -u postgres -s createdb -O $user_pg $db_name -T template0 -E UTF-8
+
+cd /home/synthese
+if [ ! -d /home/synthese/taxhub ]; then
+  wget https://github.com/PnX-SI/TaxHub/archive/1.6.5.zip
+  unzip 1.6.5.zip
+  mv TaxHub-1.6.5/ taxhub/
+  rm 1.6.5
+fi
+cd /home/synthese/taxhub
+
+if [ ! -f settings.ini ]; then
+  cp settings.ini.sample settings.ini
+fi
+
+echo "sed"
+sed -i "s,db_host=.*$,db_host=$pg_host,g" settings.ini
+sed -i "s,db_name=.*$,db_name=$pg_dbname,g" settings.ini
+sed -i "s,user_pg=.*$,user_pg=$user_pg,g" settings.ini
+sed -i "s,user_pg_pass=.*$,user_pg_pass=$user_pg_pass,g" settings.ini
+sed -i "s,db_port=.*$,db_port=$pg_port,g" settings.ini
+sed -i "s,usershub_release=.*$,usershub_release=2.1.3,g" settings.ini
+
+echo '
+<Location /taxhub>
+  ProxyPass  http://127.0.0.1:5000/ retry=0
+  ProxyPassReverse  http://127.0.0.1:5000/
+</Location>
+
+Alias "/static" "/home/synthese/taxhub/static"
+<Directory "/home/synthese/taxhub/static">
+  AllowOverride None
+  Order allow,deny
+  Allow from all
+</Directory>
+' > /etc/apache2/sites-available/taxhub.conf
+
+echo '
+RewriteEngine  on
+RewriteRule    "taxhub$"  "taxhub/"  [R]
+' >> /etc/apache2/sites-available/000-default.conf
+
+sudo a2ensite taxhub.conf
+sudo apache2ctl restart
+
+cd /home/synthese/taxhub
+mkdir var && chown -R synthese:synthese /home/synthese/taxhub
+mkdir -p /tmp/taxhub/ && chown -R synthese:synthese /tmp/taxhub
+mkdir p /tmp/usershub/ && chown -R synthese:synthese /tmp/usershub
+sed -i "s,nano.*$,#,g" install_db.sh
+sed -i "s,PnEcrins,PnX-SI,g" install_db.sh
+
+sudo passwd -d synthese
+sudo -u  synthese ./install_db.sh
+sudo -u  synthese ./install_app.sh
+cd $CURRENT_DIR
 
 if [ ! -f config/default_config.toml ]; then
   echo 'Fichier de configuration API non existant, copie du template...'
@@ -68,8 +176,8 @@ if [ $server_side = "true" ]; then
   echo "Build server side project"
   npm run build:i18n-ssr
 #  Installation de la conf
-  sudo -s cp ../geonature-service.conf /etc/supervisor/conf.d/
-  sudo -s sed -i "s%APP_PATH%${DIR}%" /etc/supervisor/conf.d/geonature-service.conf
+  cp ../geonature-service.conf /etc/supervisor/conf.d/
+  sed -i "s%APP_PATH%${DIR}%" /etc/supervisor/conf.d/geonature-service.conf
 else
   echo "Build initial du projet"
   npm run build
@@ -88,6 +196,8 @@ source $venv_path/bin/activate
 pip install --upgrade pip
 pip install -r backend/requirements.txt
 deactivate
+
+touch init_done
 
 #Création de la conf supervisor
 sudo -s cp api_geonature-service.conf /etc/supervisor/conf.d/
