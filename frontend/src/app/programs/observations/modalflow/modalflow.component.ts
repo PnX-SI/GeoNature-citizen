1517878882066f016458c2fbca8eba9a36d2ee44
Rhandy (DiD) <rhandy.grard@gmail.com>
2021-05-10 11:36:52 +0200

    feat(observations): disallow to continue without registration if program option is checked when adding observation

--- frontend/src/app/programs/observations/modalflow/modalflow.component.ts
+++ frontend/src/app/programs/observations/modalflow/modalflow.component.ts
@@ -22,6 +22,7 @@ import { AppConfig } from '../../../../conf/app.config';
 export class ModalFlowComponent implements OnInit {
     @Input('coords') coords;
     @Input('program_id') program_id;
+    @Input('registration_required') registration_required;
     @Input('form_message') form_message;
     @Input('default_image') default_image;
     @Input('updateData') updateData;
@@ -44,6 +45,7 @@ export class ModalFlowComponent implements OnInit {
         this.flowitems = this.flowService.getFlowItems({
             coords: this.coords,
             program_id: this.program_id,
+            registration_required: this.registration_required,
             form_message: this.form_message,
             default_image: this.default_image,
             updateData: this.updateData,
