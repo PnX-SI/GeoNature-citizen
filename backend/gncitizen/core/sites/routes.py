7e4dfea919542e3ed3d95399d4592ee298579333
Quentin Jouet <quentin.jouet@gmail.com>
2021-06-09 09:33:41 +0200

    Fix encoding pb loading json schema

--- backend/gncitizen/core/sites/routes.py
+++ backend/gncitizen/core/sites/routes.py
@@ -85,7 +85,7 @@ def get_site(pk):
 def get_site_jsonschema(pk):
     try:
         site = SiteModel.query.get(pk)
-        with site.site_type.form_schema.open() as json_data:
+        with site.site_type.form_schema.open(encoding='utf-8') as json_data:
             data_dict = json.load(json_data)
             return data_dict, 200
     except Exception as e:
