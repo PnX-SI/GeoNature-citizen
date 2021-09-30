92613bee169888cd41fbe4ef828665a6e5b4a79e
nobohan <juminet@gmail.com>
2021-09-30 15:04:05 +0200

    add a button for scrolling to top of the page on mobile

--- frontend/src/app/programs/sites/sites.component.ts
+++ frontend/src/app/programs/sites/sites.component.ts
@@ -103,4 +103,8 @@ export class SitesComponent extends ProgramBaseComponent implements OnInit {
     addSiteClicked() {
         this.modalFlow.first.clicked();
     }
+    toTop() {
+        document.body.scrollTop = 0; // For Safari
+        document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    }
 }
