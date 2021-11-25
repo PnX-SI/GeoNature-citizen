272e63290fbfd93228223a37dcf6f6b0ffaa4981
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-11-25 09:01:48 +0100

    fix(ref_geo): Catch exception when getting city
    Added try/except to catch errors in get_municipality function to
    prevent new observation from being created.
    The main encountered error is a DNS error caused by WSL + Docker
    apparently. Solved by restarting docker service. For this to never
    happen, this call can be done in the frontend part and the city
    can be sent to the backend

--- backend/gncitizen/utils/geo.py
+++ backend/gncitizen/utils/geo.py
@@ -25,9 +25,13 @@ def get_municipality_id_from_wkb(wkb):
         municipality = get_municipality_from_lat_long(
                                         lat=wkb["y"], 
                                         lon=wkb["x"])
+        # Chaining if conditions since the nominatim API does not return
+        # the same attributes depending on the "city"
         municipality_id = municipality.get('city', None)
         if municipality_id is None:
             municipality_id = municipality.get('town', None)
+            if municipality_id is None:
+                municipality_id = municipality.get('village', 'Non trouve')
     except Exception as e:
         current_app.logger.debug(
             "[get_municipality_id_from_wkb_point] Can't get municipality id: {}".format(
@@ -40,8 +44,13 @@ def get_municipality_id_from_wkb(wkb):
 
 def get_municipality_from_lat_long(lat: int, lon: int) -> dict:
     municipality = {}
-    resp = requests.get(f'{MUNICIPALITY_URL}?lat={lat}&lon={lon}&format=json',timeout=10)
-    if resp.ok:
-        municipality = resp.json().get('address', {})
+    try:
+        resp = requests.get(f'{MUNICIPALITY_URL}?lat={lat}&lon={lon}&format=json',timeout=10)
+        if resp.ok:
+            municipality = resp.json().get('address', {})
+    except Exception as e:
+        # Prefer passing on failure to get a Municipality than 
+        # failing on adding an observation
+        current_app.logger.warning("[get_municipality_from_lat_long] Error: %s", str(e))
+        pass
     return municipality
-
