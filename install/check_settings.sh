f43692d435f34a698271c26543feec929f7dfa67
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-11-26 10:26:33 +0100

    fix(install): add exit 1 + diff on settings
    Diff on settings to check if the user filled in the settings.ini
    Otherwise: exit 1
    Added also "1" after exit to exit properly

--- install/check_settings.sh
+++ install/check_settings.sh
@@ -5,5 +5,10 @@ if [ ! -f ./config/settings.ini ]; then
   cp ./config/settings.ini.template ./config/settings.ini
   echo "Fichier de config disponible : $DIR/config/settings.ini."
   echo "Merci de renseigner le fichier et de relancer la commande"
-  exit
+  exit 1
+elif [[ $(diff -q ./config/settings.ini.template ./config/settings.ini) = "" ]] 
+then
+  echo "Merci de renseigner le fichier config/settings.ini et de relancer la commande"
+  exit 1
 fi
+
