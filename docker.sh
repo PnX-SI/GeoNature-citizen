a05bcba86ebcce0fda194faa9aacc9c054462233
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2021-11-26 11:56:37 +0100

    fix(install): remove useless line
    Because it makes the install bug

--- docker.sh
+++ docker.sh
@@ -21,7 +21,6 @@ if ! docker --version; then
     $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update -y
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
-  sudo groupadd docker
   sudo usermod -aG docker $USER
   newgrp docker
 fi
