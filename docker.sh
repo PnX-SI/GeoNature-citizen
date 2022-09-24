34bcffea09c3aaf6849ce8e135f1e4222a65ad04
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2022-09-24 11:29:58 +0200

    docs(docker): update doc
    To match new docker-compose files

--- docker.sh
+++ docker.sh
@@ -56,4 +56,4 @@ docker-compose -f docker-compose.yml -f docker-compose.prod.yml down
 # Launch everything in detached mode
 docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build -d 
 
-# DEV : docker-compose -f docker-compose.yml -f docker-compose.dev.yml up -d
+# DEV : docker-compose up -d
