33f153d40084f7390adcbca31e342251e2a58545
lpofredc <frederic.cloitre@lpo.fr>
2023-05-15 22:49:53 +0200

    update backend start message

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,9 +2,10 @@
     Give a unique entry point for gunicorn
 """
 
-from gncitizen import __version__
 from gncitizen.utils.env import load_config
 from server import get_app
+
+from gncitizen import __version__
 
 # get the app config file
 config = load_config()
@@ -17,9 +18,9 @@ port = app.config["API_PORT"] if app.config.get("API_PORT", False) else 5002
 def main():
     print(
         f"""
-    ####################################################
-    STARTING GeoNature-Citizen version {__version__}
-    ####################################################
+####################################################
+STARTING GeoNature-Citizen version {__version__}
+####################################################
     """
     )
     app.run(host="0.0.0.0", port=port)
